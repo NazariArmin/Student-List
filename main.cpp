274b3cc9b89f0b826a88659185da480dd56279cb
armin nazari <anphoenix1999@gmail.com>
2018-12-03 17:43:36 +0330

    move source code to the ./src directory

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "List.h"
-#include "Student.h"
-int main() {
-    Student st("armin", "nazari", "4311301197");
-    List<Student> list;
-//    list.push(st);
-//    list.pop().print();
-    return 0;
-}
